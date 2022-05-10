9b545f56009d19a82d7f782cc1f0e6409b285ecc
Alexey Strelnikov <strelkov@me.com>
2022-05-10 14:50:11 +0300

    Initial Commit

---
+++ Environment Variables/Environment_VariablesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Environment_VariablesApp.swift
+//  Environment Variables
+//
+//  Created by Alexey Strelnikov on 10.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct Environment_VariablesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
